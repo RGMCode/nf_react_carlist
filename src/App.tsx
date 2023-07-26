2634b26ff41881e675579632534bed7899b41347
mrg <rgmengedoth@gmail.com>
2023-07-26 14:31:34 +0200

    push wush

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
 
 import './App.css'
 
-function CarList() {
-    const  cars = ["Mercedes", "Volvo", "BMW"]
-    return(
-        <ul>
-            {cars.map(car => <li>{car}</li>)}
-        </ul>
-    );
-}
+    // function CarList() {
+    //     const  cars = ["Mercedes", "Volvo", "BMW", "Renault", "Dacia"]
+    //
+    //     return(
+    //         <ul>
+    //             {cars.map(car => <li key={car}>{car}</li>)}
+    //         </ul>
+    //     );
+    // }
 
-function App() {
-    return <CarList></CarList>
-}
+    type Car = {
+        id: string,
+        brand: string,
+        model: string
+    }
+
+    function CarList() {
+        const  cars: Car[] = [
+            {id: "1", brand: "Marcedes", model:"GLC"},
+            {id: "2", brand: "BMW", model:"3er"},
+            {id: "3", brand: "Volvo", model:"X60"}
+            ];
+
+        return(
+            <ul>
+                {cars.map(car => <li key={car.id}>{car.brand} {car.model}</li>)}
+            </ul>
+        );
+    }
+
+    function App() {
+        return <CarList></CarList>
+    }
 
 export default App
